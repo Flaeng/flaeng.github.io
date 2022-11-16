2eb6a08e5ed968f1e4ddcb78f3653fa649c70c03
@Flaeng <flaeng@users.noreply.github.com>
2022-11-16 18:53:05 +0100

    Updated angular

--- scripts/build-ngfds.js
+++ scripts/build-ngfds.js
@@ -1,6 +1,11 @@
-const { executeAsync, copyFolder } = require('./helpers.js');
+const { executeAsync, copyFolder } = require("./helpers.js");
+const fs = require("fs");
 
 (async function () {
-    await executeAsync(`node scripts/build-showroom`, '../ngfds');
-    copyFolder(`../ngfds/showroom`, `./`);
+  fs.rmdirSync("angular", { recursive: true, force: true });
+  await executeAsync(
+    `node scripts/build-showroom.js --base-href=/angular/`,
+    "../ngfds"
+  );
+  copyFolder(`../ngfds/showroom`, `./angular`);
 })();
